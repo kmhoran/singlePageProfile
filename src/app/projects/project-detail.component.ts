26370b999b459bf85a194e28955cd525c16dbe8c
kmhoran <kevin.michael.horan@gmail.com>
2018-03-10 21:47:50 -0800

    write up portfolio project

--- src/app/projects/project-detail.component.ts
+++ src/app/projects/project-detail.component.ts
@@ -2,27 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { IConverterOptionsChangeable } from 'ngx-showdown';
 
-import { ProjectService } from "../projects/project.service";
-import { IProject } from "./project";
+import { ProjectService } from '../projects/project.service';
+import { IProject } from './project';
 
 @Component({
-  //selector: 'app-project-detail',
+  // selector: 'app-project-detail',
   templateUrl: './project-detail.component.html',
   styleUrls: ['./project-detail.component.css']
 })
 export class ProjectDetailComponent implements OnInit {
-  
+
   project: IProject;
-  
+
   allProjects: IProject[];
-    constructor(private _router: Router, 
-                private _route: ActivatedRoute, 
+    constructor(private _router: Router,
+                private _route: ActivatedRoute,
                 private _projectService: ProjectService) {
         // Avoid placing code in the constructor.
     }
-  
-  content : string;
-  markdownOptions : IConverterOptionsChangeable = {}
+
+  content: string;
+  markdownOptions: IConverterOptionsChangeable = {};
     onBack(): void {
         this._router.navigate(['/products']);
     }
@@ -30,28 +30,29 @@ export class ProjectDetailComponent implements OnInit {
 
     ngOnInit() {
 
-      let id = this._route.snapshot.paramMap.get('id');
+      const id: number = +this._route.snapshot.paramMap.get('id');
       this._projectService.getProjects()
       .subscribe(
         (projects: IProject[]) => {
           this.allProjects = projects;
 
-          for(let i = 0; i < this.allProjects.length; i++){
-            if (this.allProjects[i].id == id){
-              this.project = this.allProjects[i];
-              break;
-            }
-          }
-          
-          // In the event no project id matches request
-          if (this.project == null){
+          if (0 > id || id >= this.allProjects.length) {
             this._router.navigate(['/Home']);
           }
-          console.log("allProjects: ", projects);
-          console.log("project: ", this.project);
+          // for (let i = 0; i < this.allProjects.length; i++) {
+          //   if (this.allProjects[i].id === id) {
+          //     this.project = this.allProjects[i];
+          //     break;
+          //   }
+          // }
+
+          this.project = this.allProjects[id];
+
+          console.log('allProjects: ', projects);
+          console.log('project: ', this.project);
         }
       );
 
-      
+
   }
 }
